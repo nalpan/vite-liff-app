8ffa363d0aef368019eb28a385d93434b0a2bbe3
nal <nakata.kzhr@gmail.com>
2022-01-11 03:28:04 +0900

    docs build

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,24 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+const props = defineProps({
+  getLanguage: String,
+  getVersion: String,
+  isInClient: Boolean,
+  isLoggedIn: Boolean,
+  getLineVersion: String
+})
+
 </script>
 
 <template>
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <p>getLanguage: {{getLanguage}}</p>
+  <p>getVersion: {{getVersion}}</p>
+  <p>isInClient: {{isInClient}}</p>
+  <p>isLoggedIn: {{isLoggedIn}}</p>
+  <p>getLineVersion: {{getLineVersion}}</p>
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -5,21 +5,21 @@ import liff from '@line/liff'
 
 
 async function main(){
-  createApp(App).mount('#app')
-
   await liff.init({
     liffId: '1656795481-GomBd7X9', // Use own liffId
   })
-  
-  if(!liff.isLoggedIn()){
+
+  if(liff.isInClient() && !liff.isLoggedIn()){
     liff.login()
-  }else{
-    console.log(liff.getLanguage());
-    console.log(liff.getVersion());
-    console.log(liff.isInClient());
-    console.log(liff.isLoggedIn());
-    console.log(liff.getOS());
-    console.log(liff.getLineVersion());
+  } else {
+    const props = {
+      getLanguage: liff.getLanguage(),
+      getVersion: liff.getVersion(),
+      isInClient: liff.isInClient(),
+      isLoggedIn: liff.isLoggedIn(),
+      getLineVersion: liff.getLineVersion()
+    }
+    createApp(App, props).mount('#app')
   }
 }
 
