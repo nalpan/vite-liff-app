9082c3b8120a027b59079ffcd144dc8eea8bec71
nal <nakata.kzhr@gmail.com>
2022-01-11 02:59:09 +0900

    liff 追加

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import liff from '@line/liff'
 
-createApp(App).mount('#app')
+
+
+async function main(){
+  createApp(App).mount('#app')
+
+  await liff.init({
+    liffId: '1656795481-GomBd7X9', // Use own liffId
+  })
+  
+  if(!liff.isLoggedIn()){
+    liff.login()
+  }else{
+    console.log(liff.getLanguage());
+    console.log(liff.getVersion());
+    console.log(liff.isInClient());
+    console.log(liff.isLoggedIn());
+    console.log(liff.getOS());
+    console.log(liff.getLineVersion());
+  }
+}
+
+main()
